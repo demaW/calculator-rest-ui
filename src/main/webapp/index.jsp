adaf25a908e16c12d56a3f826b528f3501174022
Volodymyr Demchyk <demavov@gmail.com>
2015-10-19 22:54:33 +0300

    added boodstrap and created frontend for calculator

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,13 +1,55 @@
 <html>
 <head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="js/app.js"></script>
 </head>
 <body>
-	<h2>Jersey RESTful Web Application Calculator!</h2>
-	Please enter 2 whole numbers to add:
-	<input type="text" name="value1add" id ="value1add"> +
-	<input type="text" name="value2add" id ="value2add">
-	<button type="button" onclick=add()> = </button>
-	<p id = "addresult"> </p> 
+	<div class="container">
+		<div class="jumbotron">
+			<h2>Jersey RESTful Web Application Calculator!</h2>
+		</div>
+		<div class="well well-lg">
+			<p>Please enter 2 numbers to add:</p>
+			<input type="number" name="value1add" id="value1add"> + <input
+				type="number" name="value2add" id="value2add">
+			<button class="btn btn-primary btn-md" type="button" onclick=add()>=</button>
+			<p id="addresult"></p>
+		</div>
+		<br>
+		<div class="well well-lg">
+			<p>Please enter 2 numbers to subtract:</p>
+				<input type="number"
+				name="value1sub" id="value1sub"> - <input type="number"
+				name="value2sub" id="value2sub">
+			<button class="btn btn-primary btn-md" type="button" onclick=sub()>=</button>
+			<p id="subresult"></p>
+		</div>
+		<div class="well well-lg">
+			<p>Please enter 2 numbers to multiply:</p>
+			<input type="number"
+				name="value1mult" id="value1mult"> * <input type="number"
+				name="value2mult" id="value2mult">
+			<button class="btn btn-primary btn-md" type="button" onclick=mult()>=</button>
+			<p id="multresult"></p>
+		</div>
+		<div class="well well-lg">
+			<p>Please enter 2 numbers to divide:</p>
+				<input type="number"
+				name="value1div" id="value1div"> / <input type="number"
+				name="value2div" id="value2div">
+			<button class="btn btn-primary btn-md" type="button" onclick=divide()>=</button>
+			<p id="divresult"></p>
+		</div>
+	</div>
 </body>
 </html>
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,12 +1,70 @@
 function createRequest() {
-  var result = new XMLHttpRequest();
-  return result;
+	var result = new XMLHttpRequest();
+	return result;
 }
 
-function add(){
+function add() {
+	if(!validateform(document.getElementById('value1add'),document.getElementById('value2add'))){
+		return;
+	}
 	var req = createRequest();
-	var url2 = "webapi/calc/add" + "/" + document.getElementById('value1add').value + "/" + document.getElementById('value2add').value;
+	var url2 = "webapi/calc/add" + "/"
+			+ document.getElementById('value1add').value + "/"
+			+ document.getElementById('value2add').value;
 	req.open("GET", url2, false);
 	req.send();
-	document.getElementById('addresult').innerHTML =  req.responseText;
+	document.getElementById('addresult').innerHTML = req.responseText;
+}
+
+function sub() {
+	if(!validateform(document.getElementById('value1sub'),document.getElementById('value2sub'))){
+		return;
+	}
+	var req = createRequest();
+	var url2 = "webapi/calc/subtract" + "/"
+			+ document.getElementById('value1sub').value + "/"
+			+ document.getElementById('value2sub').value;
+	req.open("GET", url2, false);
+	req.send();
+	document.getElementById('subresult').innerHTML = req.responseText;
+}
+
+function mult() {
+	if(!validateform(document.getElementById('value1mult'),document.getElementById('value2mult'))){
+		return;
+	}
+	var req = createRequest();
+	var url2 = "webapi/calc/mult" + "/"
+			+ document.getElementById('value1mult').value + "/"
+			+ document.getElementById('value2mult').value;
+	req.open("GET", url2, false);
+	req.send();
+	document.getElementById('multresult').innerHTML = req.responseText;
+}
+
+function divide() {
+	if (!validateform(document.getElementById('value1div'),document.getElementById('value2div'))){
+		return;}
+	var req = createRequest();
+	var url2 = "webapi/calc/divide" + "/"
+			+ document.getElementById('value1div').value + "/"
+			+ document.getElementById('value2div').value;
+	req.open("GET", url2, false);
+	req.send();
+	document.getElementById('divresult').innerHTML = req.responseText;
+}
+
+function validateform(element1, element2) {
+	if (element1.value == null
+			|| element1.value == "") {
+		alert("A can't be blank");
+		element.value = "0";
+		return false;
+	} else if (element2.value == null
+			|| element2.value == "") {
+		alert("B can't be blank");
+		element2.innerHTML = "";
+		return false;
+	} else
+		return true;
 }
