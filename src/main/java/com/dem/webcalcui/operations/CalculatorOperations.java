e164006ddb8ee5a257f2289e1e7cd03bfbb421ae
Volodymyr Demchyk <demavov@gmail.com>
2015-10-19 22:53:50 +0300

    added another calc functions

--- src/main/java/com/dem/webcalcui/operations/CalculatorOperations.java
+++ src/main/java/com/dem/webcalcui/operations/CalculatorOperations.java
@@ -6,15 +6,35 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
 @Path("/calc")
 public class CalculatorOperations {
-	
-	@GET @Path("/add/{a}/{b}")
+
+	@GET
+	@Path("/add/{a}/{b}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String add(@PathParam("a") String a,@PathParam("b") String b){
-		return String.valueOf(Double.parseDouble(a)+Double.parseDouble(b));
+	public String add(@PathParam("a") String a, @PathParam("b") String b) {
+		return String.valueOf(Double.parseDouble(a) + Double.parseDouble(b));
 	}
-	
-	
+
+	@GET
+	@Path("/subtract/{a}/{b}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String subtract(@PathParam("a") String a, @PathParam("b") String b) {
+		return String.valueOf(Double.parseDouble(a) - Double.parseDouble(b));
+	}
+
+	@GET
+	@Path("/mult/{a}/{b}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String multiply(@PathParam("a") String a, @PathParam("b") String b) {
+		return String.valueOf(Double.parseDouble(a) * Double.parseDouble(b));
+	}
+
+	@GET
+	@Path("/divide/{a}/{b}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String divide(@PathParam("a") String a, @PathParam("b") String b) {
+		return String.valueOf(Double.parseDouble(a) / Double.parseDouble(b));
+	}
+
 }
